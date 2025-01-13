a960fd2932ecb661464c154a3644de9be9936e9f
Parkjyun <jaeyun0507@naver.com>
2025-01-14 02:25:59 +0900

    feat: LessonRoundJpaEntity 생성

---
+++ src/main/java/be/dash/dashserver/database/core/lesson/LessonRoundJpaEntity.java
@@ -0,0 +1,35 @@
+package be.dash.dashserver.database.core.lesson;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "lesson_round")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class LessonRoundJpaEntity {
+
+    @Id
+    @Column(name = "lesson_round_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "lesson_id")
+    private LessonJpaEntity lesson;
+
+    @Column(nullable = false)
+    private LocalDateTime startTime;
+
+    @Column(nullable = false)
+    private LocalDateTime endTime;
+}
