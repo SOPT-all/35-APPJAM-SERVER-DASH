d0fe379ff5416a3941225834d36b07ee66ab28d1
Parkjyun <jaeyun0507@naver.com>
2025-01-14 02:16:26 +0900

    feat: StudentGenreJpaEntity 생성

---
+++ src/main/java/be/dash/dashserver/database/core/student/StudentGenreJpaEntity.java
@@ -0,0 +1,32 @@
+package be.dash.dashserver.database.core.student;
+
+import be.dash.dashserver.database.core.genre.GenreJpaEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "student_genre")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class StudentGenreJpaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "student_id")
+    private StudentJpaEntity student;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "genre_id")
+    private GenreJpaEntity genre;
+
+}
---
+++ src/main/java/be/dash/dashserver/database/core/student/StudentGenreJpaRepository.java
@@ -0,0 +1,6 @@
+package be.dash.dashserver.database.core.student;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentGenreJpaRepository extends JpaRepository<StudentGenreJpaEntity, Long> {
+}
