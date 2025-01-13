161c099fd62442d9c79155a6a2a7cd25c9c91166
Parkjyun <jaeyun0507@naver.com>
2025-01-14 01:03:55 +0900

    feat: MemberJpaEntity 생성

---
+++ src/main/java/be/dash/dashserver/database/core/MemberJpaEntity.java
@@ -0,0 +1,50 @@
+package be.dash.dashserver.database.core;
+
+import be.dash.dashserver.core.domain.member.Role;
+import be.dash.dashserver.core.domain.member.SocialProvider;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "member")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberJpaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private SocialProvider provider;
+
+    @Column(nullable = false)
+    private String socialId;
+
+    @Column(nullable = false)
+    private String socialName;
+
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = true)
+    private String name;
+
+    @Column(nullable = true, unique = true)
+    private String phoneNumber;
+
+    @Column(nullable = true, unique = true)
+    private String nickname;
+
+}
