a0dfbbbb5753deba2623fb22690e636ccb1f365e
Parkjyun <jaeyun0507@naver.com>
2025-01-14 01:58:42 +0900

    feat: TeacherVideoJpaEntity 생성

---
+++ src/main/java/be/dash/dashserver/database/core/teacher/TeacherVideoJpaRepository.java
@@ -0,0 +1,6 @@
+package be.dash.dashserver.database.core.teacher;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeacherVideoJpaRepository extends JpaRepository<TeacherVideoJpaEntity, Long> {
+}
