e172f79c9b28f84ce797d715021931b2e5172f5a
Parkjyun <jaeyun0507@naver.com>
2025-01-16 00:12:17 +0900

    feat: reissue api 생성

--- src/main/java/be/dash/dashserver/api/core/auth/AuthController.java
+++ src/main/java/be/dash/dashserver/api/core/auth/AuthController.java
@@ -2,13 +2,17 @@ package be.dash.dashserver.api.core.auth;
 
 import be.dash.dashserver.api.core.auth.dto.LoginRequest;
 import be.dash.dashserver.api.core.auth.dto.LoginResponse;
+import be.dash.dashserver.api.core.auth.dto.ReissueResponse;
 import be.dash.dashserver.api.core.domain.member.MemberService;
 import be.dash.dashserver.core.auth.AuthService;
+import be.dash.dashserver.core.auth.ReissueService;
 import be.dash.dashserver.core.auth.Token;
 import be.dash.dashserver.core.auth.command.LoginCommand;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,13 +21,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+
     private final AuthService authService;
+    private final ReissueService reissueService;
     private final MemberService memberService;
 
     @PostMapping("/login")
-    public LoginResponse login(@RequestBody LoginRequest request) {
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
         Token token = authService.login(new LoginCommand(request.provider(), request.redirectUrl(), request.code()));
-        return LoginResponse.of(token);
+        //todo 온보딩 여부 확인
+        return ResponseEntity.ok(LoginResponse.of(token));
+    }
+
+    @PostMapping("/reissue")
+    public ResponseEntity<ReissueResponse> reissue(@RequestHeader(AUTHORIZATION_HEADER) String refreshToken) {
+        Token token = reissueService.reissue(refreshToken);
+        return ResponseEntity.ok(ReissueResponse.of(token));
     }
 
 
