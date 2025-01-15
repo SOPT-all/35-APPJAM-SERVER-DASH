d05a59fdd7cffaedc09dbd1ac5a957fc80778252
Parkjyun <jaeyun0507@naver.com>
2025-01-16 02:50:57 +0900

    refactor: @MemberId 사용하도록 변경

--- src/main/java/be/dash/dashserver/api/core/auth/AuthController.java
+++ src/main/java/be/dash/dashserver/api/core/auth/AuthController.java
@@ -3,6 +3,7 @@ package be.dash.dashserver.api.core.auth;
 import be.dash.dashserver.api.core.auth.dto.LoginRequest;
 import be.dash.dashserver.api.core.auth.dto.LoginResponse;
 import be.dash.dashserver.api.core.auth.dto.ReissueResponse;
+import be.dash.dashserver.api.support.MemberId;
 import be.dash.dashserver.core.auth.JwtTokenExtractor;
 import be.dash.dashserver.core.auth.LoginService;
 import be.dash.dashserver.core.auth.LogoutService;
@@ -11,6 +12,7 @@ import be.dash.dashserver.core.auth.Token;
 import be.dash.dashserver.core.auth.command.LoginCommand;
 import be.dash.dashserver.core.exception.DashException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,8 +27,6 @@ public class AuthController {
 
     private static final String BEARER_PREFIX = "Bearer ";
 
-    private static final String AUTHORIZATION_HEADER = "Authorization";
-
     private final LoginService loginService;
     private final ReissueService reissueService;
     private final JwtTokenExtractor jwtTokenExtractor;
@@ -40,14 +40,14 @@ public class AuthController {
     }
 
     @PostMapping("/reissue")
-    public ResponseEntity<ReissueResponse> reissue(@RequestHeader(AUTHORIZATION_HEADER) String refreshToken) {
+    public ResponseEntity<ReissueResponse> reissue(@RequestHeader(HttpHeaders.AUTHORIZATION) String refreshToken) {
         Token token = reissueService.reissue(refreshToken);
         return ResponseEntity.ok(ReissueResponse.of(token));
     }
 
     @PostMapping("/logout")
-    public ResponseEntity<Void> logout(@RequestHeader(AUTHORIZATION_HEADER) String accessToken) {
-        logoutService.logout(Long.valueOf(jwtTokenExtractor.getSubject(getToken(accessToken))));
+    public ResponseEntity<Void> logout(@MemberId Long memberId) {
+        logoutService.logout(memberId);
         return ResponseEntity.noContent().build();
     }
     private String getToken(String token) {
