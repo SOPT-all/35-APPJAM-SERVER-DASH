09e0cce1574b5bbcbdf06b8a906bd953bc726de3
yoonjuho <dbswn990@naver.com>
2025-01-16 05:26:58 +0900

    fix: Application TimeZone 설정

--- src/main/java/be/dash/dashserver/DashServerApplication.java
+++ src/main/java/be/dash/dashserver/DashServerApplication.java
@@ -1,5 +1,6 @@
 package be.dash.dashserver;
 
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DashServerApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(DashServerApplication.class, args);
 	}
 
