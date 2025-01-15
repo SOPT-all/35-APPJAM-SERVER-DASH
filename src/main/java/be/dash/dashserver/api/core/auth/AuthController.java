a9db9791b1afaad78d6da63c6ec534406b4368e2
Parkjyun <jaeyun0507@naver.com>
2025-01-16 01:52:40 +0900

    feat: 로그아웃 api 구현

--- src/main/java/be/dash/dashserver/api/core/auth/AuthController.java
+++ src/main/java/be/dash/dashserver/api/core/auth/AuthController.java
@@ -3,11 +3,13 @@ package be.dash.dashserver.api.core.auth;
 import be.dash.dashserver.api.core.auth.dto.LoginRequest;
 import be.dash.dashserver.api.core.auth.dto.LoginResponse;
 import be.dash.dashserver.api.core.auth.dto.ReissueResponse;
-import be.dash.dashserver.api.core.domain.member.MemberService;
+import be.dash.dashserver.core.auth.JwtTokenExtractor;
 import be.dash.dashserver.core.auth.LoginService;
+import be.dash.dashserver.core.auth.LogoutService;
 import be.dash.dashserver.core.auth.ReissueService;
 import be.dash.dashserver.core.auth.Token;
 import be.dash.dashserver.core.auth.command.LoginCommand;
+import be.dash.dashserver.core.exception.DashException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,11 +23,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
+    private static final String BEARER_PREFIX = "Bearer ";
+
     private static final String AUTHORIZATION_HEADER = "Authorization";
 
     private final LoginService loginService;
     private final ReissueService reissueService;
-    private final MemberService memberService;
+    private final JwtTokenExtractor jwtTokenExtractor;
+    private final LogoutService logoutService;
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
@@ -39,4 +44,18 @@ public class AuthController {
         Token token = reissueService.reissue(refreshToken);
         return ResponseEntity.ok(ReissueResponse.of(token));
     }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(@RequestHeader(AUTHORIZATION_HEADER) String accessToken) {
+        logoutService.logout(Long.valueOf(jwtTokenExtractor.getSubject(getToken(accessToken))));
+        return ResponseEntity.noContent().build();
+    }
+    private String getToken(String token) {
+        if (token.startsWith(BEARER_PREFIX)) {
+            return token.substring(BEARER_PREFIX.length());
+        } else {
+            throw new DashException("잘못된 토큰 형식입니다.");
+        }
+    }
 }
+
