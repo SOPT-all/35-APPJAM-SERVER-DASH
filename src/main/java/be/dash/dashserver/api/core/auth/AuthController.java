28aa9f8261104db9e92e79d6d84eb86675bfe8fa
Parkjyun <jaeyun0507@naver.com>
2025-01-16 05:39:35 +0900

    feat: 권한체크 적용

--- src/main/java/be/dash/dashserver/api/core/auth/AuthController.java
+++ src/main/java/be/dash/dashserver/api/core/auth/AuthController.java
@@ -4,13 +4,13 @@ import be.dash.dashserver.api.core.auth.dto.LoginRequest;
 import be.dash.dashserver.api.core.auth.dto.LoginResponse;
 import be.dash.dashserver.api.core.auth.dto.ReissueResponse;
 import be.dash.dashserver.api.support.MemberId;
-import be.dash.dashserver.core.auth.JwtTokenExtractor;
+import be.dash.dashserver.api.support.Permission;
 import be.dash.dashserver.core.auth.LoginService;
 import be.dash.dashserver.core.auth.LogoutService;
 import be.dash.dashserver.core.auth.ReissueService;
 import be.dash.dashserver.core.auth.Token;
 import be.dash.dashserver.core.auth.command.LoginCommand;
-import be.dash.dashserver.core.exception.DashException;
+import be.dash.dashserver.core.domain.member.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
@@ -25,11 +25,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
-    private static final String BEARER_PREFIX = "Bearer ";
-
     private final LoginService loginService;
     private final ReissueService reissueService;
-    private final JwtTokenExtractor jwtTokenExtractor;
     private final LogoutService logoutService;
 
     @PostMapping("/login")
@@ -39,23 +36,17 @@ public class AuthController {
         return ResponseEntity.ok(LoginResponse.of(token));
     }
 
+    @Permission(role = {Role.MEMBER, Role.TEACHER})
     @PostMapping("/reissue")
     public ResponseEntity<ReissueResponse> reissue(@RequestHeader(HttpHeaders.AUTHORIZATION) String refreshToken) {
         Token token = reissueService.reissue(refreshToken);
         return ResponseEntity.ok(ReissueResponse.of(token));
     }
 
+    @Permission(role = {Role.MEMBER, Role.TEACHER})
     @PostMapping("/logout")
     public ResponseEntity<Void> logout(@MemberId Long memberId) {
         logoutService.logout(memberId);
         return ResponseEntity.noContent().build();
     }
-    private String getToken(String token) {
-        if (token.startsWith(BEARER_PREFIX)) {
-            return token.substring(BEARER_PREFIX.length());
-        } else {
-            throw new DashException("잘못된 토큰 형식입니다.");
-        }
-    }
 }
-
