830b3e929ad1720a36bb95a83d0567b58d71408a
Parkjyun <jaeyun0507@naver.com>
2025-01-16 01:50:05 +0900

    feat: RefreshTokenJpaRepository 정의

--- src/main/java/be/dash/dashserver/core/auth/RefreshTokenRepository.java
+++ src/main/java/be/dash/dashserver/core/auth/RefreshTokenRepository.java
@@ -5,4 +5,5 @@ import java.util.Optional;
 public interface RefreshTokenRepository {
     void save(String refreshToken, long memberId);
     Optional<RefreshToken> findByRefreshToken(String refreshToken);
+    void deleteAllByMemberId(long memberId);
 }
