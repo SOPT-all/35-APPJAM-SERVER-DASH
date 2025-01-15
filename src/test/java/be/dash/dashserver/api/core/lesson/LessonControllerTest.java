43c95a2bda5925c925f631f02a39e89571bc469e
yoonjuho <dbswn990@naver.com>
2025-01-16 07:03:16 +0900

    fix: Web Mvc Config import

--- src/test/java/be/dash/dashserver/api/core/lesson/LessonControllerTest.java
+++ src/test/java/be/dash/dashserver/api/core/lesson/LessonControllerTest.java
@@ -30,7 +30,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @WebMvcTest(LessonController.class)
-@Import({LocalDateTimeConverter.class, LevelConverter.class, GenreConverter.class, SortOptionConverter.class})
+@Import({WebMvcTest.class, LocalDateTimeConverter.class, LevelConverter.class, GenreConverter.class, SortOptionConverter.class})
 class LessonControllerTest {
 
     @MockitoBean
