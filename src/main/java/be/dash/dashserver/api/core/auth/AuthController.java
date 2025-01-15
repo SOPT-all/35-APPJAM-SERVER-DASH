9093c5693f05e0db731121c6bc0d32dbca459cfa
Parkjyun <jaeyun0507@naver.com>
2025-01-16 00:16:52 +0900

    refactor: AuthService 클래스명 변경

--- src/main/java/be/dash/dashserver/api/core/auth/AuthController.java
+++ src/main/java/be/dash/dashserver/api/core/auth/AuthController.java
@@ -4,7 +4,7 @@ import be.dash.dashserver.api.core.auth.dto.LoginRequest;
 import be.dash.dashserver.api.core.auth.dto.LoginResponse;
 import be.dash.dashserver.api.core.auth.dto.ReissueResponse;
 import be.dash.dashserver.api.core.domain.member.MemberService;
-import be.dash.dashserver.core.auth.AuthService;
+import be.dash.dashserver.core.auth.LoginService;
 import be.dash.dashserver.core.auth.ReissueService;
 import be.dash.dashserver.core.auth.Token;
 import be.dash.dashserver.core.auth.command.LoginCommand;
@@ -23,13 +23,13 @@ public class AuthController {
 
     private static final String AUTHORIZATION_HEADER = "Authorization";
 
-    private final AuthService authService;
+    private final LoginService loginService;
     private final ReissueService reissueService;
     private final MemberService memberService;
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
-        Token token = authService.login(new LoginCommand(request.provider(), request.redirectUrl(), request.code()));
+        Token token = loginService.login(new LoginCommand(request.provider(), request.redirectUrl(), request.code()));
         //todo 온보딩 여부 확인
         return ResponseEntity.ok(LoginResponse.of(token));
     }
@@ -39,6 +39,4 @@ public class AuthController {
         Token token = reissueService.reissue(refreshToken);
         return ResponseEntity.ok(ReissueResponse.of(token));
     }
-
-
 }
--- src/main/java/be/dash/dashserver/core/auth/LoginService.java
+++ src/main/java/be/dash/dashserver/core/auth/LoginService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class AuthService {
+public class LoginService {
 
     private static final String BEARER_PREFIX = "Bearer ";
 
