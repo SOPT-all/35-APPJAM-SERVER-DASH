caa13cfa0d7c880fdba3faf614335238e226aefd
Parkjyun <jaeyun0507@naver.com>
2025-01-16 04:16:00 +0900

    refactor: tokenparser 분리

---
+++ src/main/java/be/dash/dashserver/core/auth/TokenParser.java
@@ -0,0 +1,18 @@
+package be.dash.dashserver.core.auth;
+
+import be.dash.dashserver.core.exception.DashException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class TokenParser {
+    private static final String BEARER_PREFIX = "Bearer ";
+    public String getToken(String token){
+        if (token.startsWith(BEARER_PREFIX)) {
+            return token.substring(BEARER_PREFIX.length());
+        } else {
+            throw new DashException("잘못된 토큰 형식입니다.");
+        }
+    }
+}
