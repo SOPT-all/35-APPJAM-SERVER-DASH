25db6758952aba6fedc81c573cf972cb815d63ea
Parkjyun <jaeyun0507@naver.com>
2025-01-17 07:38:03 +0900

    feat: 컨트롤러 테스트 작성

--- src/main/java/be/dash/dashserver/api/core/external/LocationSearchController.java
+++ src/main/java/be/dash/dashserver/api/core/external/LocationSearchController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import be.dash.dashserver.api.core.external.dto.LocationsResponse;
+import be.dash.dashserver.api.support.Permission;
+import be.dash.dashserver.core.domain.member.Role;
 import be.dash.dashserver.core.external.LocationSearchService;
 import lombok.RequiredArgsConstructor;
 
@@ -15,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 public class LocationSearchController {
     private final LocationSearchService locationSearchService;
 
+    @Permission(role = {Role.MEMBER, Role.TEACHER})
     @GetMapping("/locations")
     public ResponseEntity<LocationsResponse> getLocations(@RequestParam final String query) {
         return ResponseEntity.ok(LocationsResponse.of(locationSearchService.getLocations(query)));
