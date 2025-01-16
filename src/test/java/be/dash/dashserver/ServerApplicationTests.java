bfd1f5a47e386682d55fda1a7f5a4f76b1cbaf97
Parkjyun <jaeyun0507@naver.com>
2025-01-16 22:20:05 +0900

    feat: test yml파일 설정

--- src/test/java/be/dash/dashserver/ServerApplicationTests.java
+++ src/test/java/be/dash/dashserver/ServerApplicationTests.java
@@ -2,8 +2,10 @@ package be.dash.dashserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ServerApplicationTests {
 
     @Test
