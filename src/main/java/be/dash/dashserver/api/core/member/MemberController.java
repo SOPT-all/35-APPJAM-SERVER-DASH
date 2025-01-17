8e1a402a1b01289045b41de743fb9df9590e7c6d
Parkjyun <jaeyun0507@naver.com>
2025-01-17 21:29:35 +0900

    feat: 컨트롤러 작성

---
+++ src/main/java/be/dash/dashserver/api/core/member/MemberController.java
@@ -0,0 +1,40 @@
+package be.dash.dashserver.api.core.member;
+
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import be.dash.dashserver.api.core.member.dto.OnBoardRequest;
+import be.dash.dashserver.api.support.MemberId;
+import be.dash.dashserver.api.support.Permission;
+import be.dash.dashserver.core.domain.member.Role;
+import be.dash.dashserver.core.domain.member.command.OnboardCommand;
+import be.dash.dashserver.core.domain.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/members")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    // 선생님은 온보딩 하지 못함
+    // 멤버만 온보딩 가능
+    @Permission(role = Role.MEMBER)
+    @PostMapping("/onboard")
+    public ResponseEntity<Void> onboard(@MemberId Long memberId,
+                                        @Valid @RequestBody OnBoardRequest request) {
+        memberService.onboard(new OnboardCommand(memberId,
+                request.name(),
+                request.phoneNumber(),
+                request.nickname(),
+                request.level(),
+                request.genres(),
+                request.profileImageUrl()));
+        return ResponseEntity.ok().build();
+    }
+
+}
