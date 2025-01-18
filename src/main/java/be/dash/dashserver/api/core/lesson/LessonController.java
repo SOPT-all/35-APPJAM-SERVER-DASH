6b295b146bfeb5ce6be921911601fd3c26380402
Parkjyun <jaeyun0507@naver.com>
2025-01-19 03:02:31 +0900

    feat: 수업 생성 api 작성

--- src/main/java/be/dash/dashserver/api/core/lesson/LessonController.java
+++ src/main/java/be/dash/dashserver/api/core/lesson/LessonController.java
@@ -1,16 +1,23 @@
 package be.dash.dashserver.api.core.lesson;
 
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import be.dash.dashserver.api.core.lesson.dto.LessonFilterRequest;
 import be.dash.dashserver.api.core.lesson.dto.LessonResponses;
+import be.dash.dashserver.api.core.lesson.dto.createLessonRequest;
+import be.dash.dashserver.api.support.MemberId;
+import be.dash.dashserver.api.support.Permission;
 import be.dash.dashserver.core.domain.lesson.LessonSortOption;
 import be.dash.dashserver.core.domain.lesson.Lessons;
 import be.dash.dashserver.core.domain.lesson.service.LessonService;
+import be.dash.dashserver.core.domain.member.Role;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -29,5 +36,14 @@ public class LessonController {
                 lessonFilterRequest.endDate(),
                 sortOption);
         return ResponseEntity.ok(new LessonResponses(searched));
+    }
+
+    @Permission(role = Role.TEACHER)
+    @PostMapping
+    public ResponseEntity<Void> create(@MemberId Long memberId,
+                                       @Valid @RequestBody createLessonRequest request) {
+        lessonService.createLesson(request.toCommand(memberId));
+        return ResponseEntity.ok().build();
+
     }
 }
