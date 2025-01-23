e540ee3dc96d9f61c017b74992004d5c9ccf0022
YoonJuHo <dbswn990@naver.com>
2025-01-24 01:49:37 +0900

    refactor: 호환 가능 확장자 파일 추가 및 파일 최대 용량 제한 (#82)

--- src/main/java/be/dash/dashserver/api/support/GlobalExceptionHandler.java
+++ src/main/java/be/dash/dashserver/api/support/GlobalExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 import be.dash.dashserver.api.exception.DashApiException;
 import be.dash.dashserver.api.exception.ErrorMessage;
@@ -18,6 +19,7 @@ import be.dash.dashserver.core.exception.ForbiddenException;
 import be.dash.dashserver.core.exception.NotFoundException;
 import be.dash.dashserver.core.log.LogForm;
 import lombok.extern.slf4j.Slf4j;
+import software.amazon.awssdk.services.s3.model.S3Exception;
 
 @Slf4j
 @RestControllerAdvice
@@ -82,6 +84,18 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorMessage> handleNotFoundException(NotFoundException e) {
         log.warn("handleNotFoundException in GlobalExceptionHandler throw {} : {}", e.getClass(), e.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorMessage(e.getMessage()));
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<ErrorMessage> handleMaxUploadSizeExceededException(MaxUploadSizeExceededException e) {
+        log.warn("handleMaxUploadSizeExceededException in GlobalExceptionHandler throw {} : {}", e.getClass(), e.getMessage());
+        return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE).body(new ErrorMessage(e.getMessage()));
+    }
+
+    @ExceptionHandler(S3Exception.class)
+    public ResponseEntity<ErrorMessage> handleS3Exception(S3Exception e) {
+        log.warn("handleS3Exception in GlobalExceptionHandler throw {} : {}", e.getClass(), e.getMessage());
+        return ResponseEntity.badRequest().body(new ErrorMessage(e.getMessage()));
     }
 
     @ExceptionHandler(DashApiException.class)
